f377ca457ed94dfd8a441f6b1a0eb8ff997869da
htaiwan <htaiwan@oath.com>
2019-10-27 07:33:38 +0800

    [03] Adding Material Icons with the Icon Widget

--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -161,6 +161,54 @@ class MiCardApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+ 886 975055210',
+                      style: TextStyle(
+                        fontFamily: 'Cairo',
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'htaiwan@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Cairo',
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),

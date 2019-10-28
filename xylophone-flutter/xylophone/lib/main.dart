2365d515bea85f01dc63c4011bcfc4f05b08cf9f
htaiwan <htaiwan@oath.com>
2019-10-29 06:56:56 +0800

    [06] XylophoneApp - Add starter

---
+++ xylophone-flutter/xylophone/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(xylophoneApp());
+
+class xylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:
+    );
+  }
+}
+

e25ffdccb9a20a99af7604969bc9ae4c236ec227
htaiwan <htaiwan@oath.com>
2019-10-30 06:50:08 +0800

    [07] Building a Score Keeper

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -25,6 +25,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> _scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -61,7 +63,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  _scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                });
               },
             ),
           ),
@@ -79,12 +83,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  _scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: _scoreKeeper,
+        ),
       ],
     );
   }

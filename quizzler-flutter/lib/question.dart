2ba891db571ac64079ce3f9c86ec1ee6e0940d1b
htaiwan <htaiwan@oath.com>
2019-10-30 11:28:46 +0800

    [07] Using constructors

--- quizzler-flutter/lib/question.dart
+++ quizzler-flutter/lib/question.dart
@@ -2,6 +2,9 @@ class Question {
   String questionText;
   bool questionAnswer;
 
+  // 另外一種constructors的更簡易寫法
+  // https://dart.dev/guides/language/language-tour#constructors
+  // Question( {this.questionText, this.questionAnswer} )
   Question({String q, bool a}) {
     questionText = q;
     questionAnswer = a;

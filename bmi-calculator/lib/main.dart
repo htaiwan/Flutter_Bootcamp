d60f1a0e661f6256d4c95ae9cd717b3688cafd78
htaiwan <htaiwan@oath.com>
2019-10-30 13:47:27 +0800

    [09] How to Use Flutter Themes

--- bmi-calculator/lib/main.dart
+++ bmi-calculator/lib/main.dart
@@ -6,6 +6,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // https://flutter.dev/docs/cookbook/design/themes
+      theme: ThemeData.dark().copyWith(
+        // 沒有定義以外的東西，都是用ThemeData所定義的東西
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        accentColor: Colors.red,
+      ),
       home: InputPage(),
     );
   }

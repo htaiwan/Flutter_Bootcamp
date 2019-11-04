7cbe2e70689b62c4c7e28c1d79aef75eeffc569d
htaiwan <htaiwan@oath.com>
2019-11-04 11:52:42 +0800

    [10] Getting Actual Weather Data from the OpenWeatherMap API

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+
+const apiKey = 'dd06ea5d0e566bd70ceb4c4414128079';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,17 +10,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double longitude;
+  double latitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
+    longitude = location.longitude;
+    latitude = location.latitude;
+
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+    NetworkHelper networkHelper = NetworkHelper(url: url);
+    var responseData = await networkHelper.getData();
+
+    print(responseData);
   }
 
   @override
@@ -27,7 +40,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
-            getLocation();
           },
           child: Text('hello'),
         ),

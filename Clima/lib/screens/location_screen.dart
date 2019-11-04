dc76e7aec07b16255abfd38bddd53b3c016505b9
htaiwan <htaiwan@oath.com>
2019-11-04 14:37:46 +0800

    [10] Creating and Styling a TextField Widget for Text Entry

--- Clima/lib/screens/location_screen.dart
+++ Clima/lib/screens/location_screen.dart
@@ -2,6 +2,8 @@ import 'package:clima/services/weather.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:flutter/material.dart';
 
+import 'city_screen.dart';
+
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.weatherData});
 
@@ -75,7 +77,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,

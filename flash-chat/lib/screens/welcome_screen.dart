548cbbd48705fa82673a00e96e9c026febb2ba0c
htaiwan <htaiwan@oath.com>
2019-11-06 15:11:13 +0800

    [12] Prepackaged Flutter Animations

--- flash-chat/lib/screens/welcome_screen.dart
+++ flash-chat/lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 import 'login_screen.dart';
@@ -30,9 +31,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),

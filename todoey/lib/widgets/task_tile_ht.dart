69ed7cfe00587309553058cf70c67e8fd861b10f
htaiwan <htaiwan@oath.com>
2019-11-13 17:26:46 +0800

    [13] What is State and How do we Manage it?

--- todoey/lib/widgets/task_tile_ht.dart
+++ todoey/lib/widgets/task_tile_ht.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTileHT extends StatelessWidget {
-  const TaskTileHT({
-    Key key,
-  }) : super(key: key);
+class TaskTileHT extends StatefulWidget {
+  @override
+  _TaskTileHTState createState() => _TaskTileHTState();
+}
+
+class _TaskTileHTState extends State<TaskTileHT> {
+  bool isChecked = false;
+  void checkBoxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('項目1'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        '項目1',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBoxHT(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkBoxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckBoxHT extends StatelessWidget {
+  // 要將child widget狀態回傳到parent使用
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckBoxHT({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }

c342c1642566898443f4daf32098a1028db4a39f
htaiwan <htaiwan@oath.com>
2019-11-15 17:13:56 +0800

    [13] The ListView Builder

--- todoey/lib/widgets/task_list_ht.dart
+++ todoey/lib/widgets/task_list_ht.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 
+import '../models/task.dart';
 import '../widgets/task_tile_ht.dart';
 
-class TaskListHT extends StatelessWidget {
-  const TaskListHT({
-    Key key,
-  }) : super(key: key);
+class TaskListHT extends StatefulWidget {
+  @override
+  _TaskListHTState createState() => _TaskListHTState();
+}
+
+class _TaskListHTState extends State<TaskListHT> {
+  List<Task> tasks = [
+    Task(name: "喝飲料"),
+    Task(name: "吃雞排"),
+    Task(name: "去運動"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTileHT(),
-        TaskTileHT(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTileHT(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (bool checkboxState) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
--- todoey/lib/widgets/task_tile_ht.dart
+++ todoey/lib/widgets/task_tile_ht.dart
@@ -1,47 +1,25 @@
 import 'package:flutter/material.dart';
 
-class TaskTileHT extends StatefulWidget {
-  @override
-  _TaskTileHTState createState() => _TaskTileHTState();
-}
+class TaskTileHT extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
 
-class _TaskTileHTState extends State<TaskTileHT> {
-  bool isChecked = false;
-  void checkBoxCallback(bool checkboxState) {
-    setState(() {
-      isChecked = checkboxState;
-    });
-  }
+  TaskTileHT({this.taskTitle, this.isChecked, this.checkboxCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        '項目1',
+        taskTitle,
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: TaskCheckBoxHT(
-        checkboxState: isChecked,
-        toggleCheckboxState: checkBoxCallback,
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
       ),
-    );
-  }
-}
-
-class TaskCheckBoxHT extends StatelessWidget {
-  // 要將child widget狀態回傳到parent使用
-  final bool checkboxState;
-  final Function toggleCheckboxState;
-
-  TaskCheckBoxHT({this.checkboxState, this.toggleCheckboxState});
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: checkboxState,
-      onChanged: toggleCheckboxState,
     );
   }
 }

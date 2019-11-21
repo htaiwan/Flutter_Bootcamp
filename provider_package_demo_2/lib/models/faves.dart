68063debd2420a0f633b2feb0157a64dbe6c5a08
htaiwan <htaiwan@oath.com>
2019-11-21 17:27:34 +0800

    [15] Create a ChangeNotifier

---
+++ provider_package_demo_2/lib/models/faves.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:trilogies/models/films.dart';
+
+class FavesModel extends ChangeNotifier {
+  final FilmsModel _films;
+  final List<int> _episodeIds;
+
+  FavesModel(this._films, FavesModel previous)
+      : assert(_films != null),
+        _episodeIds = previous?._episodeIds ?? [];
+
+  List<Film> get films => _episodeIds.map((id) => _films.getById(id)).toList();
+
+  Film getByPosition(int position) => films[position];
+
+  void add(Film film) {
+    _episodeIds.add(film.episodeId);
+    // ChangeNotifier的method
+    notifyListeners();
+  }
+
+  void remove(Film film) {
+    _episodeIds.remove(film.episodeId);
+    notifyListeners();
+  }
+
+  int get length => _episodeIds.length;
+}

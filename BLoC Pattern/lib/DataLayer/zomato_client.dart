9ff11b2f55d7dc8a93bc96cd4995e42ed95ac363
htaiwan <htaiwan@oath.com>
2019-11-26 17:44:51 +0800

    [16] Get a Zomato API Key

--- BLoC Pattern/lib/DataLayer/zomato_client.dart
+++ BLoC Pattern/lib/DataLayer/zomato_client.dart
@@ -38,7 +38,7 @@ import 'location.dart';
 import 'restaurant.dart';
 
 class ZomatoClient {
-  final _apiKey = 'PASTE YOUR API KEY HERE';
+  final _apiKey = '64c68ecaaf324b91ad864c5d2e2614a6';
   final _host = 'developers.zomato.com';
   final _contextRoot = 'api/v2.1';
 

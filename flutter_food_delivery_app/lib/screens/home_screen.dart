958d2def083ccdaf90d7f0a9cc219b9538cb5dd2
htaiwan <htaiwan@oath.com>
2019-12-09 17:24:17 +0800

    [17] Recent Orders

--- flutter_food_delivery_app/lib/screens/home_screen.dart
+++ flutter_food_delivery_app/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_app/Widgets/recent_orders.dart';
 import 'package:flutter_food_delivery_app/data/data.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -24,6 +25,43 @@ class _HomeScreenState extends State<HomeScreen> {
               'cart (${currentUser.cart.length})',
               style: TextStyle(color: Colors.white, fontSize: 20.0),
             ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                hintText: "Search Food or Resturants",
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+          RecentOrders(),
           )
         ],
       ),

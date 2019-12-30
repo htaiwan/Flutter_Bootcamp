fd610a80ee3121606e52e7c0c9205b5d17e73ba1
htaiwan <htaiwan@oath.com>
2019-12-30 17:00:51 +0800

    [18] Sliver App Bar and Sliver List

--- flutter_budget_app/lib/screens/home_screen.dart
+++ flutter_budget_app/lib/screens/home_screen.dart
@@ -8,6 +8,44 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            forceElevated: true,
+//            floating: true,
+            pinned: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 100.0,
+                  color: Colors.red,
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

6553a4d8e00b95a748b8cef52b26a479a4d9d2af
htaiwan <htaiwan@oath.com>
2020-01-09 12:59:40 +0800

    [18] Layout Builder - Remaining Money Bars

--- flutter_budget_app/lib/screens/home_screen.dart
+++ flutter_budget_app/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/Widgets/bar_chart.dart';
 import 'package:flutter_budget_ui/data/data.dart';
+import 'package:flutter_budget_ui/helper/color_helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
 import 'package:flutter_budget_ui/models/expense_model.dart';
 
@@ -13,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   _buildCategory(Category category, double totalAmountSpent) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.all(20.0),
       height: 100.0,
       width: double.infinity,
       decoration: BoxDecoration(
@@ -41,6 +43,39 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
               ),
             ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final double maxBarWidth = constraints.maxWidth;
+              final double percent =
+                  (category.maxAmount - totalAmountSpent) / category.maxAmount;
+              double barWidth = percent * maxBarWidth;
+              if (barWidth < 0) {
+                barWidth = 0;
+              }
+              return Stack(
+                children: <Widget>[
+                  Container(
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  Container(
+                    height: 20.0,
+                    width: barWidth,
+                    decoration: BoxDecoration(
+                      color: getColor(context, percent),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  )
+                ],
+              );
+            },
           ),
         ],
       ),

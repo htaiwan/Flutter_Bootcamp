42f947ad709d515e70afbc38a9bed3cf28714e5f
htaiwan <htaiwan@oath.com>
2020-01-10 16:34:59 +0800

    [18] Custom Painter - Radial Progress Bar

--- flutter_budget_app/lib/screens/category_screen.dart
+++ flutter_budget_app/lib/screens/category_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/Widgets/radial_painter.dart';
+import 'package:flutter_budget_ui/helper/color_helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
+import 'package:flutter_budget_ui/models/expense_model.dart';
 
 class CategoryScreen extends StatefulWidget {
   final Category category;
@@ -12,6 +15,13 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmount = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmount += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmount;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -40,7 +50,22 @@ class _CategoryScreenState extends State<CategoryScreen> {
                         offset: Offset(0, 2),
                         blurRadius: 6.0),
                   ]),
-            )
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),

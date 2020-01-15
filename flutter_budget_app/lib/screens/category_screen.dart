c3da63e62abf6713a89945151d96a7c87c3a528e
htaiwan <htaiwan@oath.com>
2020-01-15 17:08:20 +0800

    [18] Expense List

--- flutter_budget_app/lib/screens/category_screen.dart
+++ flutter_budget_app/lib/screens/category_screen.dart
@@ -13,6 +13,53 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  _buildExpenses() {
+    List<Widget> expenseList = [];
+
+    widget.category.expenses.forEach((Expense expense) {
+      expenseList.add(
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          height: 80.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0)
+              ]),
+          child: Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  expense.name,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                Text(
+                  '~\$${expense.cost.toStringAsFixed(2)}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20.0,
+                      color: Colors.red),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+
+    return Column(
+      children: expenseList,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double totalAmount = 0;
@@ -66,6 +113,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 ),
               ),
             ),
+            _buildExpenses(),
           ],
         ),
       ),
